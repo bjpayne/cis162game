a3b4109451fcbdd99ea8276ce359a44da6a3e956
Ben Payne <benj@minpayne.com>
2017-04-04 15:02:22 -0400

    dev commit

--- src/game/Item.java
+++ src/game/Item.java
@@ -1,11 +1,10 @@
 package game;
 
-/*************************************
- * The Item class.
- *
- * @author benjaminp
- * @version 4/4/2017.
- *************************************/
+/*****************************************************************
+The Item class.
+@author benjaminp
+@version 4/4/2017.
+******************************************************************/
 public class Item {
 
     /** The items name */
@@ -20,14 +19,13 @@ public class Item {
     /** Items edibility */
     private boolean isEdible;
 
-    /***************************************************************************
-     * Initialize a new Item
-     *
-     * @param name - The items name.
-     * @param description - The items description.
-     * @param weight - The items weight.
-     * @param isEdible - Whether the item is edible.
-     **************************************************************************/
+    /*****************************************************************
+    Initialize a new Item
+    @param name The items name.
+    @param description The items description.
+    @param weight The items weight.
+    @param isEdible Whether the item is edible.
+    *****************************************************************/
     public Item (
         final String name,
         final String description,
@@ -43,74 +41,66 @@ public class Item {
         this.isEdible = isEdible;
     }
 
-    /***************************************************************************
-     * Get the items name
-     *
-     * @return - The items name
-     **************************************************************************/
+    /*****************************************************************
+    Get the items name
+    @return The items name
+    *****************************************************************/
     public String getName() {
         return name;
     }
 
-    /***************************************************************************
-     * Set the items name
-     *
-     * @param name - The items name
-     **************************************************************************/
+    /*****************************************************************
+    Set the items name
+    @param name The items name
+    *****************************************************************/
     public void setName(String name) {
         this.name = name;
     }
 
-    /***************************************************************************
-     * Set the items description
-     *
-     * @return - the items description
-     **************************************************************************/
+    /*****************************************************************
+    Set the items description
+    @return the items description
+    *****************************************************************/
     public String getDescription() {
         return description;
     }
 
-    /***************************************************************************
-     * Set the items description
-     *
-     * @param description - the items description
-     **************************************************************************/
+    /*****************************************************************
+    Set the items description
+    @param description the items description
+    *****************************************************************/
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /***************************************************************************
-     * Get the items weight
-     *
-     * @return - the items weight
-     **************************************************************************/
+    /*****************************************************************
+    Get the items weight
+    @return the items weight
+    *****************************************************************/
     public int getWeight() {
         return weight;
     }
 
-    /***************************************************************************
-     * Set the items weight
-     *
-     * @param weight - the items weight
-     **************************************************************************/
+    /*****************************************************************
+    Set the items weight
+    @param weight the items weight
+    *****************************************************************/
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
-    /***************************************************************************
-     * Check whether the item is edible
-     *
-     * @return - whether the item is edible
-     **************************************************************************/
+    /*****************************************************************
+    Check whether the item is edible
+    @return whether the item is edible
+    *****************************************************************/
     public boolean isEdible() {
         return isEdible;
     }
 
-    /***************************************************************************
-     * Set the items edibility
-     *
-     * @param edible - whether the item is edible or not
-     **************************************************************************/
+    /*****************************************************************
+    Set the items edibility
+    @param edible whether the item is edible or not
+    *****************************************************************/
     public void setEdible(boolean edible) {
         isEdible = edible;
     }
